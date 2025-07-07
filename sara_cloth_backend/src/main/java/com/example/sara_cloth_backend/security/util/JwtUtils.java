847a47ff5f9ec5202b98666d29413116da5a2d09
Tharinda Pamindu <ict2022100@as.rjt.ac.lk>
2025-07-07 19:10:18 +0530

    Completed JwtUtils.java

---
+++ sara_cloth_backend/src/main/java/com/example/sara_cloth_backend/security/util/JwtUtils.java
@@ -0,0 +1,78 @@
+package com.example.sara_cloth_backend.security.util;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtUtils.class);
+
+    @Value("${sara.secure_key}")
+    private String jwtSecret;
+
+    @Value("${sara.expiration}")
+    private Long jwtExpiration;
+
+    public String generateToken(Authentication authentication) {
+
+        UserDetails userPrincipal = (UserDetails) authentication.getPrincipal();
+
+        String role = userPrincipal.getAuthorities().stream()
+                .findFirst()
+                .map(GrantedAuthority::getAuthority)
+                .orElse("USER");
+
+        return Jwts.builder()
+                .setSubject(userPrincipal.getUsername())
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + jwtExpiration))
+                .setIssuer("SARA")
+                .signWith(key(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public Boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key()).build().parseClaimsJwt(token);
+            return true;
+        } catch (MalformedJwtException e) {
+            LOGGER.error("Invalid JWT token {} ", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            LOGGER.error("Expired JWT token {} ", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            LOGGER.error("Unsupported JWT token {} ", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            LOGGER.error("JWT payload is empty {}", e.getMessage());
+        }
+        return false;
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder().setSigningKey(key()).build().parseClaimsJwt(token).getBody().getSubject();
+    }
+
+    public SimpleGrantedAuthority extractAuthorities(String token) {
+        Claims claims = Jwts.parserBuilder().setSigningKey(key()).build().parseClaimsJwt(token).getBody();
+
+        return new SimpleGrantedAuthority(claims.get("role", String.class));
+    }
+
+}
